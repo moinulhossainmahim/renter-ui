6f663841619b0440d6207a2179e056bb7175caa7
mhossain2 <moinulhossainmahim@gmail.com>
2025-01-03 17:51:37 +0600

    enhance: updated profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -56,28 +56,6 @@ const Profile = () => {
               </p>
             </div>
           </div>
-          {/* verified info  */}
-          <div className="bg-[#1C1917] p-5 mt-5 rounded-2xl ">
-            <div>
-              <p className="text-lg font-semibold">Verified Info</p>
-              <div className="flex items-center gap-5 py-4">
-                <FaCircleCheck className="text-xl" />
-                <p className="text-[#EBEEF2]">Email Address</p>
-              </div>
-            </div>
-            <div className="my-5 bg-gray-400 border-b-[1px]" />
-            <div>
-              <p className="text-lg font-semibold">Verify Identity</p>
-              <p className="text-[#EBEEF2] py-2 text-sm">
-                Before Rent or Host on{" "}
-                <span className="text-[#E11D48]">Homyz</span> you need to verify
-                your identity first
-              </p>
-              <button className="bg-[#E11D48] px-4 py-2 w-full my-5 rounded-lg">
-                Get Verified
-              </button>
-            </div>
-          </div>
           {/* <div className="bg-[#1C1917] p-5 mt-5 rounded-2xl">
             <button className="bg-[#E11D48] px-4 py-2 w-full my-5 rounded-lg">
               LogOut
